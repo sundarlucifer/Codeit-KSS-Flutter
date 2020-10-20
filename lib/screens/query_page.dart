9ad98b63fd464a370ec2b75a11b869a8b5c235db
sundarlucifer <sundarlucifer@gmail.com>
2020-10-20 15:45:09 +0530

    Added date range

--- lib/screens/query_page.dart
+++ lib/screens/query_page.dart
@@ -9,27 +9,31 @@ class QueryPage extends StatefulWidget {
 
 class _QueryPageState extends State<QueryPage> {
   String studentName;
-  DateTime date;
+  DateTime _fromDate;
+  DateTime _toDate;
+  bool _isRange = false;
+  bool _isLoading = false;
 
-  var p1 = {'txt': '', 'err': ''};
-  var p2 = {'txt': '', 'err': ''};
-
-  // String p1 = '', p2 = '', e1 = '', e2 = '';
+  List<Widget> _programs = [];
 
   @override
   void initState() {
     super.initState();
 
     studentName = studentNames.first;
-    date = DateTime.now();
+    _fromDate = DateTime.now();
+    _toDate = DateTime.now();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Query Page')),
+      appBar: AppBar(
+        title: Text('Query Page', style: TextStyle(color: Colors.white)),
+      ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               height: 200.0,
@@ -45,30 +49,9 @@ class _QueryPageState extends State<QueryPage> {
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Text(
-                'Program 1: ' + p1['err'],
-                style: TextStyle(fontSize: 32.0),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(12.0),
-              decoration: BoxDecoration(),
-              child: Text(p1['txt']),
-            ),
-            Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Text(
-                'Program 2: ' + p2['err'],
-                style: TextStyle(fontSize: 32.0),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(12.0),
-              decoration: BoxDecoration(),
-              child: Text(p2['txt']),
-            ),
+            SizedBox(height: 12.0),
+            if (_isLoading) Center(child: CircularProgressIndicator()),
+            if (!_isLoading) ..._programs,
           ],
         ),
       ),
@@ -87,43 +70,117 @@ class _QueryPageState extends State<QueryPage> {
   }
 
   Widget _dateSelector() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        OutlineButton(
+          child: Text('Range / Date'),
+          onPressed: () => setState(() => _isRange = !_isRange),
+        ),
+        SizedBox(width: 12),
+        _fromDateSelector(),
+        _toDateSelector(),
+      ],
+    );
+  }
+
+  Widget _fromDateSelector() {
     return FlatButton(
-      child: Text(this.date.toString().substring(0, 10)),
+      child: Text(_fromDate.toString().substring(0, 10)),
       onPressed: () => showDatePicker(
         context: context,
-        initialDate: this.date,
+        initialDate: _fromDate,
         firstDate: DateTime.parse('2020-10-09'),
         lastDate: DateTime.now(),
-      ).then((val) => setState(() => this.date = val ?? this.date)),
+      ).then((val) => setState(() {
+            _fromDate = val ?? _fromDate;
+            if (!_isRange) _toDate = _fromDate;
+          })),
     );
+  }
+
+  Widget _toDateSelector() {
+    return _isRange
+        ? FlatButton(
+            child: Text(_toDate.toString().substring(0, 10)),
+            onPressed: () => showDatePicker(
+              context: context,
+              initialDate: _toDate,
+              firstDate: DateTime.parse('2020-10-09'),
+              lastDate: DateTime.now(),
+            ).then((val) => setState(() {
+                  _toDate = val ?? _toDate;
+                })),
+          )
+        : Center();
   }
 
   Widget _button() {
     var url = 'https://raw.githubusercontent.com/' +
         gitID[studentName] +
-        '/Codeit-KSS/main/' +
-        date.toString().substring(0, 10).replaceAll('-', '_');
+        '/Codeit-KSS/main/';
+
     return RaisedButton(
-      child: Text('Get Programs'),
-      onPressed: () {
-        get(url + '_01', p1);
-        get(url + '_02', p2);
+      child: Text(
+        'Get Programs',
+        style: TextStyle(color: Colors.white),
+      ),
+      color: Theme.of(context).primaryColor,
+      onPressed: () async {
+        setState(() => _isLoading = true);
+        DateTime iter = _fromDate;
+        DateTime end = _toDate.add(Duration(days: 1));
+        _programs.clear();
+        setState(() => _programs);
+
+        while (iter.isBefore(end)) {
+          _programs.add(_dateHeader(iter));
+          await get(
+              url, iter.toString().substring(0, 10).replaceAll('-', '_'), '01');
+          await get(
+              url, iter.toString().substring(0, 10).replaceAll('-', '_'), '02');
+          iter = iter.add(Duration(days: 1));
+        }
+        setState(() {
+          _isLoading = false;
+          return _programs;
+        });
       },
     );
   }
 
-  void get(url, p) async {
-    var response = await http.get(url);
+  Future get(url, date, pID) async {
+    var response = await http.get(url + date + '_$pID');
     if (response.statusCode == 200) {
-      setState(() {
-        p['txt'] = response.body;
-        p['err'] = '';
-      });
+      _programs.add(_header('Program $pID:'));
+      _programs.add(_content(response.body));
     } else {
-      setState(() {
-        p['err'] = 'File not found';
-        p['txt'] = '';
-      });
+      _programs.add(_header('Program $pID: Not found'));
     }
+  }
+
+  _dateHeader(DateTime date) {
+    return Padding(
+      padding: EdgeInsets.all(12.0),
+      child: Text(
+        date.toString().substring(0, 10),
+        style: TextStyle(fontSize: 24.0, color: Colors.green),
+      ),
+    );
+  }
+
+  _header(String text) {
+    return Padding(
+      padding: EdgeInsets.all(12.0),
+      child: Text(text, style: TextStyle(fontSize: 20.0)),
+    );
+  }
+
+  _content(String text) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(),
+      child: Text(text),
+    );
   }
 }
